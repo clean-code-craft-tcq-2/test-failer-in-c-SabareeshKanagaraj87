2338a563607340748d283160826b31258646b258
Sabareesh_Kanagaraj <34646057+SabareeshKanagaraj87@users.noreply.github.com>
2022-02-02 01:44:36 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 #include <assert.h>
 
+const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+static char PrintString[100] = "";
+
+
+static char* GetPrintString(int PairNumber, char majorColor, char minorColor)
+{
+    sprintf(PrintString, "%d|%s|%s|\n", PairNumber, majorColor, minorColor);
+    return PrintString;
+}
+
 int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+			printf("%s",GetPrintString(i * 5 + j, majorColor[i], minorColor[i]));
         }
     }
     return i * j;
@@ -15,7 +25,11 @@ int printColorMap() {
 
 int main() {
     int result = printColorMap();
+    char ResultString = GetPrintString(1,"White","Blue");
     assert(result == 25);
+	assert(ResultString == "1|White|Blue")
+	
+	
     printf("All is well (maybe!)\n");
     return 0;
 }
